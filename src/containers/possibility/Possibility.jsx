8894b5310500bd9be7f91adf7c303d5affb8aeff
King David <45265898+DaWei8@users.noreply.github.com>
2022-12-10 04:43:28 +0100

    fixed some issues with the repsonsiveness on the desktop view

--- src/containers/possibility/Possibility.jsx
+++ src/containers/possibility/Possibility.jsx
@@ -6,7 +6,7 @@ const Possibility = () => {
   return (
     <div className="gpt3__possibility">
       <div className="gpt3_possibility-container">
-        <img src={possibility} alt="possibility" />
+        <div><img src={possibility} alt="possibility" /></div>
         <div className="gpt3__possibility-text">
           <p className="gpt3__possibility-request-top">Request Early Access to Get Started</p>
           <h2 className="gpt3__possibility-heading">The possibilities are beyond your imagination</h2>
